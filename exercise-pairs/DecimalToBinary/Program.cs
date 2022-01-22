320bff758a575fe0c1907f4036cfd23e80c8b07a
Craig Royer <craigkroyer@gmail.com>
2022-01-22 11:05:56 -0500

    Craig-edits-decimal-converter

--- exercise-pairs/DecimalToBinary/Program.cs
+++ exercise-pairs/DecimalToBinary/Program.cs
@@ -6,7 +6,20 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Dave! \nPlease enter a series of base-10 integers (separated by spaces):");
+            string userInput = Console.ReadLine();
+            string[] decimalInputArray = userInput.Split(" ");
+
+            //13 % 2 = 1
+            //(13 / 2 = 6)
+            //6 % 2 = 0
+            //(6 / 2 = 3)
+            //3 % 2 = 1
+            //(3 / 2 = 1)
+            //1 % 2 = 1
+
+            //    1101
         }
+
     }
 }
--- exercise-pairs/Fibonacci/Program.cs
+++ exercise-pairs/Fibonacci/Program.cs
@@ -49,13 +49,6 @@ namespace Fibonacci
 
 
             //Create a 10 number long version of the Fibbonacci Sequence
-
-
-
-
-
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
