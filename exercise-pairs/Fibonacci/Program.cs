e606dbe22ea9b304154be929005024a6b03cf9eb
Jason Davis <tripod6720@gmail.com>
2022-01-22 09:33:58 -0500

    Jason First Driver - Devin Taking Over - Fibonacci Work Push 1

--- exercise-pairs/Fibonacci/Program.cs
+++ exercise-pairs/Fibonacci/Program.cs
@@ -6,6 +6,43 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Greetings User! Please enter an integer value:");
+            
+            string userInputString = Console.ReadLine();
+            int userInputInt = int.Parse(userInputString);
+            int fibonacciSmaller = 1;
+            int fibonacciLarger = 1;
+            int fibonacciCurrent = 0;
+           
+            string fibSequence = "0, 1";
+
+            //Special Case <+0
+            if(userInputInt <= 0)
+            {
+                Console.WriteLine(fibSequence);
+            }
+            else
+            {
+                while(userInputInt > fibonacciLarger - 1)
+                {
+                    fibonacciCurrent = fibonacciSmaller + fibonacciLarger;
+                    fibonacciSmaller = fibonacciLarger;
+                    fibonacciLarger = fibonacciCurrent;
+
+                    fibSequence += ", " + fibonacciLarger;
+
+                }
+
+                Console.WriteLine(fibSequence);
+
+            }
+            //Adjust String for next Fibonacci number
+            //Loop to add numbers to string until the Fibonacci > UserInput
+
+            //Print the string 
+
+            //Startover
+
 
             //Create a 10 number long version of the Fibbonacci Sequence
 
