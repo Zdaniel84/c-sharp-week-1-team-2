fd05befc68ca0bc90ada27aeb5010534744d8b72
Zachary Daniel <zdaniel84@gmail.com>
2022-01-22 12:33:19 -0500

    Zac decimalToBinary - driving -day 1 break

--- exercise-pairs/DecimalToBinary/Program.cs
+++ exercise-pairs/DecimalToBinary/Program.cs
@@ -19,6 +19,62 @@ namespace DecimalToBinary
             //1 % 2 = 1
 
             //    1101
+            
+            int convertedValuePlaceholder = 0; //declare temp variables
+            int integerDivisionResults = 1;
+            int moduloResult = 0;
+            string returnToUser = "";
+           
+            for (int i = 0; i < decimalInputArray.Length; i++) 
+            {
+                
+                int convertedValue = int.Parse(decimalInputArray[i]); // convert to integer array
+                //converted value to binary
+                
+                {//modulo math
+                    convertedValuePlaceholder = convertedValue;
+                    while (integerDivisionResults != 0)
+
+                    {
+                        moduloResult = convertedValue % 2;
+
+                        if (moduloResult == 1)
+                        {
+                            returnToUser = "1" + returnToUser;
+                        }
+                        else if (moduloResult == 0)
+                        {
+                            returnToUser = "0" + returnToUser;
+                        }
+                        integerDivisionResults = convertedValue / 2;
+                        //convertedValuePlaceholder = convertedValue;
+                        convertedValue = convertedValue / 2;
+
+
+
+                    }
+                    Console.WriteLine(convertedValuePlaceholder + " in binary is: " + returnToUser);
+                    Console.WriteLine(returnToUser);
+                    returnToUser = "";
+                    //toss result to string
+
+                    //integer division
+
+
+
+                }
+                
+
+            }
+
+
+
+
+
+
+            //start for loop
+            //parse to ints
+            //binary conversion
         }
 
     }
