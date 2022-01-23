c0d8b46f71fe04a308ed6d4f4ef151dcf96299e5
Jason Davis <tripod6720@gmail.com>
2022-01-22 23:19:30 -0500

    Binary converter works, needs some serious tweaking - 22 Jan 2022 2319

--- exercise-pairs/DecimalToBinary/Program.cs
+++ exercise-pairs/DecimalToBinary/Program.cs
@@ -6,9 +6,11 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Dave! \nPlease enter a series of base-10 integers (separated by spaces):");
-            string userInput = Console.ReadLine();
-            string[] decimalInputArray = userInput.Split(" ");
+            //Console.WriteLine("Hello, Dave! \nPlease enter a series of base-10 integers (separated by spaces):");
+            string userInput = "";
+            // string[] decimalInputArray = userInput.Split(" ");
+            string userPlayAgainChoice = "";
+            bool userPlayAgainChoiceBool = true;
 
             //13 % 2 = 1
             //(13 / 2 = 6)
@@ -24,50 +26,86 @@ namespace DecimalToBinary
             int integerDivisionResults = 1;
             int moduloResult = 0;
             string returnToUser = "";
-           
-            for (int i = 0; i < decimalInputArray.Length; i++) 
+            int convertedValue = 0;
+            bool continueVariable = true;
+            
+            while (continueVariable == true)
             {
-                
-                int convertedValue = int.Parse(decimalInputArray[i]); // convert to integer array
-                //converted value to binary
-                
-                {//modulo math
-                    convertedValuePlaceholder = convertedValue;
-                    while (integerDivisionResults != 0)
+                Console.WriteLine("Hello, Dave! \nPlease enter a series of base-10 integers (separated by spaces):");
+                userInput = Console.ReadLine();
+                string[] decimalInputArray = userInput.Split(" ");
+              //  userPlayAgainChoice = "y";
+                //userPlayAgainChoiceBool;
 
-                    {
-                        moduloResult = convertedValue % 2;
 
-                        if (moduloResult == 1)
+                for (int i = 0; i < decimalInputArray.Length; i++)
+                {
+                    //Reset Variables
+                    integerDivisionResults = 1;
+                    returnToUser = "";
+
+
+
+                    convertedValue = int.Parse(decimalInputArray[i]); // convert to integer array
+                                                                      //converted value to binary
+
+                    {//modulo math
+                        convertedValuePlaceholder = convertedValue;
+                        while (integerDivisionResults != 0)
+
                         {
-                            returnToUser = "1" + returnToUser;
+                            moduloResult = convertedValue % 2;
+
+                            if (moduloResult == 1)
+                            {
+                                returnToUser = "1" + returnToUser;
+                            }
+                            else if (moduloResult == 0)
+                            {
+                                returnToUser = "0" + returnToUser;
+                            }
+                            integerDivisionResults = convertedValue / 2;
+                            //convertedValuePlaceholder = convertedValue;
+                            convertedValue = convertedValue / 2;
+
+
+
                         }
-                        else if (moduloResult == 0)
-                        {
-                            returnToUser = "0" + returnToUser;
-                        }
-                        integerDivisionResults = convertedValue / 2;
-                        //convertedValuePlaceholder = convertedValue;
-                        convertedValue = convertedValue / 2;
+                        Console.WriteLine(convertedValuePlaceholder + " in binary is: " + returnToUser);
+                        //  Console.WriteLine(returnToUser);
+                        //   returnToUser = "";
+                        //toss result to string
+
+                        //integer division
 
 
 
                     }
-                    Console.WriteLine(convertedValuePlaceholder + " in binary is: " + returnToUser);
-                    Console.WriteLine(returnToUser);
-                    returnToUser = "";
-                    //toss result to string
-
-                    //integer division
-
 
 
                 }
-                
 
+                //Copied the play again code from Fibonacci
+                //-----------------------------------------
+                Console.WriteLine("Would you like to try another number? Enter y for yes or n for no");
+                userPlayAgainChoice = Console.ReadLine();
+
+                //Clear Screen to Make it Pretty
+                Console.Clear();
+
+                if (userPlayAgainChoice == "y")
+                {
+                    userPlayAgainChoice = "true";
+                }
+                else if (userPlayAgainChoice == "n")
+                {
+                    userPlayAgainChoice = "false";
+                    Console.WriteLine("Have a Nice Day!\r\nPress Return to Exit");
+                    Console.ReadLine();
+                }
+
+                continueVariable = bool.Parse(userPlayAgainChoice);
             }
-
-
 
 
 
